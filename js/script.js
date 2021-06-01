b84b6994400095500730010100c387e025c91f2e
Volkan Basman <53094850+vbasman@users.noreply.github.com>
2021-06-01 22:22:13 +0200

    applied fixes

--- js/script.js
+++ js/script.js
@@ -115,14 +115,13 @@ $(function () { // Same as document.addEventListener("DOMContentLoaded"...
         // Hint: you need to surround the chosen category short name with something before inserting
         // it into the home html snippet.
 
-        chosenCategoryShortName = "'" + chosenCategoryShortName + "'";
-        var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, "randomCategoryShortName", chosenCategoryShortName);
+        var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, 'randomCategoryShortName', "'" + chosenCategoryShortName + "'");
 
 
         // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
         // Use the existing insertHtml function for that purpose. Look through this code for an example
         // of how to do that.
-        insertHtml('#main-content', homeHtmlToInsertIntoMainPage);
+        insertHtml("#main-content", homeHtmlToInsertIntoMainPage);
 
       },
       false); // False here because we are getting just regular HTML from the server, so no need to process JSON.
